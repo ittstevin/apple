f6ce13ff481fdd2f79b5128ede0a076f45419514
ittstevin <tevingichoya@gmail.com>
2025-12-11 15:04:12 +0300

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+const navLinks = [
+    {label: "Store"},
+    {label: "Mac"},
+    {label: "iPad"},
+    {label: "iPhone"},
+    {label: "Watch"},
+    {label: "AirPods"},
+    {label: "TV & Home"},
+    {label: "Entertainment"},
+    {label: "Accessories"},
+]
+
+export { navLinks }
